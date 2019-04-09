158c6e31fdc6290a190265d8ed196a40c1d0170b
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-04-09 11:51:03 +0800

    Ok

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth import logout, login
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .serializers import UserSerializer
 
@@ -19,8 +21,14 @@ class LoginView(APIView):
         username = request.data.get("username")
         password = request.data.get("password")
         user = authenticate(username=username, password=password)
-        if user:
+        if user.is_active:
+            login(request, user)
             return Response({"token": user.auth_token.key})
         else:
             return Response({"error": "Wrong Credentials"},
                             status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view
+def log_out(request):
+    logout(request)
