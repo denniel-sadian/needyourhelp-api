5277e05881a07fb99ab7ff36c37ab39b392ad72a
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-04-09 16:26:19 +0800

    Ok

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey
+from .models import Interviewee
+from .models import TextAnswerable
+from .models import Opinion
+# from .models import MultipleChoice
+# from .models import Choice
+
+
+class IntervieweeAdmin(admin.ModelAdmin):
+    list_display = ('date_interviewed', 'survey')
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'date_started', 'done', 'user')
+
+
+class OpinionInline(admin.StackedInline):
+    model = Opinion
+    extra = 3
+
+
+class TextAnswerableAdmin(admin.ModelAdmin):
+    list_display = ('survey', 'text')
+    inlines = [OpinionInline]
+
+
+"""
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class MultipleChoiceAdmin(admin.ModelAdmin):
+    list_display = ('survey', 'text')
+    inlines = [ChoiceInline]
+"""
+
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(TextAnswerable, TextAnswerableAdmin)
+admin.site.register(Interviewee, IntervieweeAdmin)
+# admin.site.register(MultipleChoice, MultipleChoiceAdmin)
--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Survey(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    date_started = models.DateField(auto_now=True)
+    done = models.BooleanField(default=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Interviewee(models.Model):
+    firt_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_interviewed = models.DateField(auto_now=True)
+    survey = models.ForeignKey(to=Survey, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.firt_name} {self.last_name}"
+
+
+class Question(models.Model):
+    survey = models.ForeignKey(to=Survey, on_delete=models.CASCADE)
+
+
+class Response(models.Model):
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    by = models.ForeignKey(to=Interviewee, on_delete=models.CASCADE)
+
+
+class TextAnswerable(Question):
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+
+
+class Opinion(Response):
+    text = models.CharField(max_length=255)
+
+
+"""
+class MultipleChoice(Question):
+    text = models.CharField(max_length=255)
+    choose_all = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(Response):
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+"""
