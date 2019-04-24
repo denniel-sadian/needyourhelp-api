ba4234649b51b2c584194b27bcc002a4ef374242
Denniel Luis Sadian <sadiandenniel@gmail.com>
2019-04-24 20:34:06 +0800

    Ok

--- needyourhelp/settings.py
+++ needyourhelp/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6pn3n5n+mnn1^rlia))#b%=_h2o@w=dyv=&dw7g$7nsj6qa^3i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
